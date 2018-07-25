ae2e8d040ccdfe56b171cacdbe2639bd5a0a2fbf
Brook Gagnon <brook@pikalabs.com>
2018-07-25 20:19:48 +0000

    initial commit

---
+++ js/data_and_statistics.js
@@ -0,0 +1,123 @@
+OBModules.DataAndStatistics = new function()
+{
+
+  this.init = function()
+  {
+    OB.Callbacks.add('ready',0,OBModules.DataAndStatistics.initMenu);
+    if(!document.getElementById('obmodules-chart-js')) $('head').append('<script id="obmodules-chart-js" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.bundle.min.js"></script>');
+  }
+
+  this.initMenu = function()
+  {
+    OB.UI.addSubMenuItem('admin','Data & Statistics','data_and_statistics',OBModules.DataAndStatistics.open,140,'data_statistics_module');
+  }
+  
+  this.chartColors = ['#3366CC','#DC3912','#FF9900','#109618','#990099','#3B3EAC','#0099C6','#DD4477','#66AA00','#B82E2E','#316395','#994499','#22AA99','#AAAA11','#6633CC','#E67300','#8B0707','#329262','#5574A6','#3B3EAC'];
+  
+  this.open = function()
+  {
+    OB.UI.replaceMain('modules/data_and_statistics/data_and_statistics.html');
+    
+    OB.API.post('dataandstatistics', 'get_all', {}, function(response)
+    {
+      var media_metadata = ['media_categories','media_genres','media_countries','media_languages'];
+      var media_status = ['media_status','media_approved','media_owner'];
+      var media_formats = ['media_types','media_audio_formats','media_video_formats','media_image_formats'];
+      var playlist_status = ['playlist_type','playlist_status'];
+    
+      $.each(response.data, function(index,data)
+      {
+        if(media_metadata.indexOf(index)>=0) var container = 'media_metadata';
+        else if(media_status.indexOf(index)>=0) var container = 'media_status';
+        else if(media_formats.indexOf(index)>=0) var container = 'media_formats';
+        else if(playlist_status.indexOf(index)>=0) var container = 'playlist_status';
+        else return;
+      
+        // skip sections with no data
+        if(data.length==0) return;
+      
+        var heading = index.replace(/_/g,' ');
+      
+        var $section = $('<div class="data_and_statistics-section" id="data_and_statistics-'+index+'"></div>');
+        $section.append('<h3><a href="javascript:OBModules.DataAndStatistics.modal(\''+index+'\');">'+heading+'</a></h3>');
+        $section.append('<canvas width="1024" height="768"><canvas>');
+        $('#data_and_statistics-'+container+'_container').append($section);
+        
+        $('#data_and_statistics-'+index).data('heading',heading);
+        $('#data_and_statistics-'+index).data('data',data);
+        
+        var values = [];
+        var labels = [];
+        $.each(data, function(index2,data2)
+        {
+          values.push(data2.count);
+          labels.push(data2.name);
+          if(values.length==5) return false;
+        });
+
+        var ctx = $('#data_and_statistics-'+index+' canvas')[0].getContext('2d');
+        var data = {
+          datasets: [{
+              data: values,
+              backgroundColor: OBModules.DataAndStatistics.chartColors
+          }],
+          labels: labels
+        };
+        
+        new Chart(ctx, {
+          responsive:true,
+          maintainAspectRatio: true,
+          type: 'pie',
+          data: data,
+          options: { 
+            animation: {duration: 0}, 
+            cutoutPercentage: 0, 
+            legend: {display: true, labels: { fontColor: '#ffffff'}} 
+          }
+        });
+      });
+    });    
+  }
+  
+  this.modal = function(what)
+  {
+    OB.UI.openModalWindow('modules/data_and_statistics/modal.html');
+    $('#data_and_statistics-modal_heading').text( $('#data_and_statistics-'+what).data('heading') );
+
+    var data = $('#data_and_statistics-'+what).data('data');
+
+    $thead = $('#data_and_statistics-modal_table thead tr');
+    $tbody = $('#data_and_statistics-modal_table tbody');
+
+    // get total count and total duration
+    var total_count = 0;
+    var total_duration = 0.0;
+    $.each(data, function(index,row)
+    {
+      total_count+=parseInt(row['count']);
+      if(row['duration']) total_duration+=parseFloat(row['duration']);
+    });
+
+    // output headings
+    $.each(data[0], function(name,value)
+    {
+      if(what=='media_image_formats' && name=='duration') return true;
+      $thead.append( $('<th></th>').text(name) );
+    });
+    
+    // output data
+    $.each(data, function(index,row)
+    {
+      var $row = $('<tr></tr>');
+      $.each(row, function(name, value)
+      {
+        if(what=='media_image_formats' && name=='duration') return true;
+        if(name=='duration') value=secsToTime(value,'hms')+' ('+Math.round(parseInt(value)*10000/total_duration,2)/100+'%)';
+        if(name=='count') value+=' ('+Math.round(parseInt(value)*10000/total_count,2)/100+'%)';
+        $row.append( $('<td></td>').text(value) );
+      });
+      $tbody.append($row);
+    });
+  }
+}
+  
---
+++ models/dataandstatistics_model.php
@@ -0,0 +1,178 @@
+<?php
+
+class DataAndStatisticsModel extends OBFModel
+{
+  public function media_types()
+  {
+    $this->db->query('SELECT type as name,count(*) AS count,sum(duration) as duration FROM media GROUP BY type ORDER BY count DESC');
+    $rows = $this->db->assoc_list();    
+    return [];
+    return $rows;
+  }
+  
+  public function media_categories()
+  {
+    $this->db->query('SELECT media_categories.name as name,count(*) as count,sum(duration) as duration
+        FROM media 
+        LEFT JOIN media_categories ON media.category_id=media_categories.id 
+        GROUP BY media.category_id ORDER BY count DESC');
+        
+    $rows = $this->db->assoc_list();
+    
+    $missing = 0;
+    $missing_duration = 0;
+    foreach($rows as $index=>$row)
+    {
+      if($row['name']===null)
+      { 
+        $missing += $row['count'];
+        $missing_duration += $row['duration'];
+        unset($rows[$index]);
+      }
+    }
+    
+    if($missing>0) $rows[] = ['name'=>'(missing category)','count'=>$missing,'duration'=>$missing_duration];
+    
+    return $rows;
+  }
+  
+  public function media_genres()
+  {
+    $this->db->query('SELECT media_categories.name as category_name,media_genres.name as name,count(*) as count,sum(duration) as duration 
+        FROM media 
+        LEFT JOIN media_genres ON media.genre_id=media_genres.id 
+        LEFT JOIN media_categories ON media_genres.media_category_id=media_categories.id
+        GROUP BY media.genre_id ORDER BY count DESC');
+        
+    $rows = $this->db->assoc_list();
+    
+    $missing = 0;
+    $missing_duration = 0;
+    foreach($rows as $index=>$row)
+    {
+      if($row['name']===null)
+      { 
+        $missing += $row['count'];
+        $missing_duration += $row['duration'];
+        unset($rows[$index]);
+      }
+      else
+      {
+        $rows[$index]['name'] = $row['category_name'].': '.$row['name'];
+        unset($rows[$index]['category_name']);
+      }
+    }
+    
+    if($missing>0) $rows[] = ['name'=>'(missing genre)','count'=>$missing, 'duration'=>$missing_duration];
+    
+    return $rows;
+  }
+  
+  public function media_countries()
+  {
+    $this->db->query('SELECT media_countries.name as name,count(*) as count,sum(duration) as duration 
+        FROM media 
+        LEFT JOIN media_countries ON media.country_id=media_countries.id 
+        GROUP BY media.country_id ORDER BY count DESC');
+        
+    $rows = $this->db->assoc_list();
+    
+    $missing = 0;
+    $missing_duration = 0;
+    foreach($rows as $index=>$row)
+    {
+      if($row['name']===null)
+      { 
+        $missing += $row['count'];
+        $missing_duration += $row['duration'];
+        unset($rows[$index]);
+      }
+    }
+    
+    if($missing>0) $rows[] = ['name'=>'(missing country)','count'=>$missing,'duration'=>$missing_duration];
+    
+    return $rows;
+  }
+  
+  public function media_languages()
+  {
+    $this->db->query('SELECT media_languages.name as name,count(*) as count,sum(duration) as duration
+        FROM media 
+        LEFT JOIN media_languages ON media.language_id=media_languages.id 
+        GROUP BY media.language_id ORDER BY count DESC');
+        
+    $rows = $this->db->assoc_list();
+    
+    $missing = 0;
+    $missing_duration = 0;
+    foreach($rows as $index=>$row)
+    {
+      if($row['name']===null)
+      { 
+        $missing += $row['count'];
+        $missing_duration += $row['duration'];
+        unset($rows[$index]);
+      }
+    }
+    
+    if($missing>0) $rows[] = ['name'=>'(missing language)','count'=>$missing,'duration'=>$missing_duration];
+    
+    return $rows;
+  }
+  
+  public function media_audio_formats()
+  {
+    $this->db->query('SELECT format as name,count(*) AS count,sum(duration) as duration FROM media WHERE type="audio" and format!="" GROUP BY format ORDER BY count DESC');
+    $rows = $this->db->assoc_list();
+    return $rows;
+  }
+  
+  public function media_video_formats()
+  {
+    $this->db->query('SELECT format as name,count(*) AS count,sum(duration) as duration FROM media WHERE type="video" and format!="" GROUP BY format ORDER BY count DESC');
+    $rows = $this->db->assoc_list();
+    return $rows;
+  }
+  
+  public function media_image_formats()
+  {
+    $this->db->query('SELECT format as name,count(*) AS count,sum(duration) as duration FROM media WHERE type="image" and format!="" GROUP BY format ORDER BY count DESC');
+    $rows = $this->db->assoc_list();
+    return $rows;
+  }
+  
+  public function media_status()
+  {
+    $this->db->query('SELECT status as name,count(*) AS count,sum(duration) as duration FROM media GROUP BY status ORDER BY count DESC');
+    $rows = $this->db->assoc_list();
+    return $rows;
+  }
+  
+  public function media_approved()
+  {
+    $this->db->query('SELECT (CASE WHEN is_approved <> 0 THEN "approved" ELSE "not approved" END) as name,count(*) AS count,sum(duration) as duration FROM media GROUP BY is_approved ORDER BY count DESC');
+    $rows = $this->db->assoc_list();
+    return $rows;
+  }
+  
+  public function media_owner()
+  {
+    $this->db->query('SELECT (CASE WHEN is_copyright_owner <> 0 THEN "copyright owner" ELSE "not copyright owner" END) as name,count(*) AS count,sum(duration) as duration FROM media GROUP BY is_copyright_owner ORDER BY count DESC');
+    $rows = $this->db->assoc_list();
+    return $rows;
+  }
+  
+  public function playlist_type()
+  {
+    $this->db->query('SELECT type as name,count(*) AS count FROM playlists GROUP BY type ORDER BY count DESC');
+    $rows = $this->db->assoc_list();    
+    return $rows;
+  }
+  
+  public function playlist_status()
+  {
+    $this->db->query('SELECT status as name,count(*) AS count FROM playlists GROUP BY status ORDER BY count DESC');
+    $rows = $this->db->assoc_list();
+    return $rows;
+  }
+}
