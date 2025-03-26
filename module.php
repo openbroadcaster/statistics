9956a4935291716dc760eea1b05863a6a015a914
Sanae <92342730+hinanaya@users.noreply.github.com>
2025-03-26 01:16:17 +0000

    rely on fk constraints for deleting permissions, added purge method

--- module.php
+++ module.php
@@ -13,28 +13,27 @@ class DataAndStatisticsModule extends OBFModule
 
 	public function install()
 	{
-      // add permissions data for this module
-      $this->db->insert('users_permissions', [
-        'category'=>'administration',
-        'description'=>'data & statistics module',
-        'name'=>'data_statistics_module'
-      ]);
+        // add permissions data for this module
+        $this->db->insert('users_permissions', [
+            'category'=>'administration',
+            'description'=>'data & statistics module',
+            'name'=>'data_statistics_module'
+        ]);
 
-      return true;
+        return true;
 	}
 
 	public function uninstall()
 	{
-      // remove permissions data for this module
-      $this->db->where('name','data_statistics_module');
-      $permission = $this->db->get_one('users_permissions');
+        // remove permissions data for this module
+        $this->db->where('name','data_statistics_module');
+        $this->db->delete('users_permissions');
 
-      $this->db->where('permission_id',$permission['id']);
-      $this->db->delete('users_permissions_to_groups');
-
-      $this->db->where('id',$permission['id']);
-      $this->db->delete('users_permissions');
-
-      return true;
+        return true;
 	}
+
+    public function purge()
+    {
+        return true;
+    }
 }
